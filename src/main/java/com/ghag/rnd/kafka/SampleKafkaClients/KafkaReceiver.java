82c25b805caa2d1679bcfb0143abca3b8549eece
Ganesh Ghag <ganesh.ghag@gmail.com>
2016-10-05 10:11:41 +0530

    added proper comments and wrap up, for partition based pub and sub

--- src/main/java/com/ghag/rnd/kafka/SampleKafkaClients/KafkaReceiver.java
+++ src/main/java/com/ghag/rnd/kafka/SampleKafkaClients/KafkaReceiver.java
@@ -2,8 +2,10 @@ package com.ghag.rnd.kafka.SampleKafkaClients;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Properties;
 
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -18,7 +20,7 @@ public class KafkaReceiver {
 		props.put("bootstrap.servers", KafkaSender.KAFKA_BROKER_URL);
 		props.put("group.id", "test");
 		props.put("enable.auto.commit", "true");
-		props.put("auto.offset.reset", "earliest");
+		//props.put("auto.offset.reset", "earliest");
 		props.put("auto.commit.interval.ms", "1000");
 		props.put("session.timeout.ms", "30000");
 		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
@@ -27,14 +29,31 @@ public class KafkaReceiver {
 		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
 		System.out.println("after new consumer");
 		
-		//consumer.subscribe(Arrays.asList("test"));
-		//System.out.println("size="+consumer.assignment().size());
+		//default subscribe
+//		consumer.subscribe(Arrays.asList("test");
 		
+//		consumer.subscribe(Arrays.asList("test"), new ConsumerRebalanceListener() {
+//			
+//			@Override
+//			public void onPartitionsRevoked(Collection<TopicPartition> arg0) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//			
+//			@Override
+//			public void onPartitionsAssigned(Collection<TopicPartition> arg0) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//		});
+
+		//partition based subscribe
 		ArrayList<TopicPartition> list = new ArrayList<TopicPartition>();
 		list.add(new TopicPartition("test", 0));		
-		consumer.assign(list);
+		
 		System.out.println("size="+consumer.assignment().size());
 
+		//programmatic "auto.offset.reset", "earliest"
 		consumer.seekToBeginning(list);
 
 		while (true) {
